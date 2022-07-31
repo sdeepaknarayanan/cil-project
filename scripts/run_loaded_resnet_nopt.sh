f7a56b072e8d5a4d1400c70e29d718af2b820ef4
Deepak Narayanan <deepaknarayanan120199@gmail.com>
2022-07-31 02:43:10 +0200

    Delete run_loaded_resnet_nopt.sh

--- scripts/run_loaded_resnet_nopt.sh
+++
@@ -1,2 +0,0 @@
-python finetune.py --train_image_path ../gmap_data/images --train_mask_path ../gmap_data/groundtruth --batch_size 4 --epochs 200 --loss dice --name resnet_nopt_loaded --aug 1 --model resnet --load_path resnet_nopt_model/best_model_192
-
