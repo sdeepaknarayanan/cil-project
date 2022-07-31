7ff4cc0289278001d14b778e62228a5a9336a6a3
Deepak Narayanan <deepaknarayanan120199@gmail.com>
2022-07-31 02:36:56 +0200

    Create run_xception_ethz.sh

---
+++ scripts/run_xception_ethz.sh
@@ -0,0 +1,1 @@
+python train.py --train_image_path ../training/images --train_mask_path ../training/groundtruth --batch_size 4 --epochs 200 --loss dice --name xception_ethz --model xception --pretr 1
