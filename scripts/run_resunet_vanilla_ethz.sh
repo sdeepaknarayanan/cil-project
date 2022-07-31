a585641f19c726ad5c87ab7958cb04757135cb23
Deepak Narayanan <deepaknarayanan120199@gmail.com>
2022-07-31 02:42:59 +0200

    Delete run_resunet_vanilla_ethz.sh

--- scripts/run_resunet_vanilla_ethz.sh
+++
@@ -1,2 +0,0 @@
-python train.py --train_image_path ../training/images --train_mask_path ../training/groundtruth --batch_size 6 --epochs 400 --loss dice --name resunet_vanilla_ethz --aug 1 --model resunet --pretr 1
-
