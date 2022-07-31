24abdeba94e8cc77b7c96d6a2f06bf68e3bac66a
Deepak Narayanan <deepaknarayanan120199@gmail.com>
2022-07-31 02:43:26 +0200

    Delete run_xception_gmap_wd.sh

--- scripts/run_xception_gmap_wd.sh
+++
@@ -1,1 +0,0 @@
-python train.py --train_image_path ../gmap_data/images --train_mask_path ../gmap_data/groundtruth --batch_size 4 --epochs 250 --loss dice --name xception_gmap --aug 1 --model xception --pretr 1 --wd 1e-5
